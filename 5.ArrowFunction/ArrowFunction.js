f8b9526fe6a65d5a1bbadb7ba739704489cacb41
sungwon-H <bbh653@naver.com>
2020-12-22 16:11:42 +0900

    Add Arrowfunction example

--- 5.ArrowFunction/ArrowFunction.js
+++ 5.ArrowFunction/ArrowFunction.js
@@ -25,3 +25,36 @@ function not1(x){
 //not1의 내용을 간결하게 표현
 const not2 = x => !x;
 
+console.log('====================');
+
+
+const relationship1 ={
+    name:'won',
+    friends: ['jin','woo','soo'],
+    logFriends: function(){
+        const that = this; // this는 realtionship1을 가리킨다
+        this.friends.forEach(function (friend){ // realtionship1의 객체 안에 friends배열을 순차적으로 실행
+            console.log(that.name, friend);
+            /*
+            won jin
+            won woo
+            won soo
+            */ 
+        });
+    }
+}
+relationship1.logFriends();
+
+console.log('====================');
+
+const relationship2 = {
+    name: 'won',
+    friends:['jin','woo','soo'],
+    logFriends(){
+        this.friends.forEach(friend =>{
+            console.log(this.name, friend);
+        });
+    },
+};
+
+relationship2.logFriends();
